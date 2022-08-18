f1a891b231613a394a87f41502cc6f8096d900c8
dmrunal08 <111482876+dmrunal08@users.noreply.github.com>
2022-08-18 14:55:05 +0530

    Again

--- Sketch/main.js
+++ Sketch/main.js
@@ -1,22 +1,6 @@
-var violinsound;
-
-function preload(){
-  violinsound=loadSound('Sounds/violin.wav');
-}
-
 function setup() {
   createCanvas(400, 400);
 }
-  
-function mouseMoved() {
-  // playing a sound file on a user gesture
-  // is equivalent to `userStartAudio()`
-  if(!violinsound.isPlaying())
-  {
-    violinsound.play();
-  }  
-}
-
 
 function draw() {
   background(220);
@@ -24,20 +8,30 @@ function draw() {
   
   strokeWeight(0.5);
   
+  //line(0,0,172,-122);
+  //line(0,0,172,122);
+  //line(0,0,-172,122);
+  //line(0,0,-172,-122);
+  
+  //line(0,0,-122,-172);
+  //line(0,0,-122,172);
+  //line(0,0,122,-172);
+  //line(0,0,122,172);
+  
   for(i=0;i<20;i++)
   {
   stroke('#CE545F');
-  curve(0,mouseY,0,0,172,-122+6*i,172,mouseY);
-  curve(0,-mouseY,0,0,172,122-6*i,172,-mouseY);
-  curve(0,-mouseY,0,0,-172,122-6*i,172,-mouseY);
-  curve(0,mouseY,0,0,-172,-122+6*i,172,mouseY);
+  line(0,0,172,-122+6*i);
+  line(0,0,172,122-6*i);
+  line(0,0,-172,122-6*i);
+  line(0,0,-172,-122+6*i);
   }//Loop for horizontal rectangle
   
   for(i=0;i<20;i++){
-  curve(0,mouseY,0,0,-122+6*i,172,122,mouseY);
-  curve(0,mouseY,0,0,122-6*i,172,-122,mouseY);
-  curve(0,-mouseY,0,0,-122+6*i,-172,122,mouseY);
-  curve(0,-mouseY,0,0,122-6*i,-172,-122,mouseY); 
+  line(0,0,-122+6*i,172);
+  line(0,0,122-6*i,172);
+  line(0,0,-122+6*i,-172);
+  line(0,0,122-6*i,-172); 
   } //Loop for vertical rectangle
   
   line(122,0,122,-172);
@@ -91,8 +85,9 @@ function draw() {
   noFill();
   strokeWeight(1.5);
   stroke('#1F1F1F');
-  quad(mouseX,0,0,122,-mouseX,0,0,-122);
-  quad(0,mouseY,-122,0,0,-mouseY,122,0);//Black quadrilaterals
+  quad(172,0,0,122,-172,0,0,-122);
+  quad(0,172,-122,0,0,-172,122,0);//Black quadrilaterals
 }
+
 
   
